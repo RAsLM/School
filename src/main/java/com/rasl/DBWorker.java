11d312a79ed943c7b1e28bf5b9d281897ee483ec
ruslan <aslanov.r.m@gmail.com>
2017-12-25 18:01:05 +0300

    Initial commit

---
+++ src/main/java/com/rasl/DBWorker.java
@@ -0,0 +1,23 @@
+package com.rasl;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBWorker {
+    private static final String URL = "jdbc:mysql://localhost:3306/school";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private Connection connection;
+
+    public DBWorker(){
+        try{
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Соединение с БД Установлено!");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/rasl/Main.java
@@ -0,0 +1,8 @@
+package com.rasl;
+
+public class Main {
+    private static DBWorker DBWORKER = new DBWorker();
+
+    public static void main(String[] args) {
+    }
+}
