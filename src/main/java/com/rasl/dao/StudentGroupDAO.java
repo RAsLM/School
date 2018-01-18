b78eae02316e812cf6057532a2eab28baeed0b8f
ruslan <aslanov.r.m@gmail.com>
2018-01-18 13:19:42 +0300

    The test code in the method main was deleted

--- src/main/java/com/rasl/dao/StudentGroupDAO.java
+++ src/main/java/com/rasl/dao/StudentGroupDAO.java
@@ -5,21 +5,21 @@ import com.rasl.pojo.Student;
 import java.util.List;
 
 public class StudentGroupDAO implements DAO {
+
+    public void create(Object obj) {
+
+    }
+
+    public void update(Object obj) {
+
+    }
+
     public Object getOne(int id) {
         return null;
     }
 
     public List<Object> getAll() {
         return null;
-    }
-
-    @Override
-    public void update(Object obj) {
-
-    }
-
-    public void create(Object obj) {
-
     }
 
     public void delete(int id) {
